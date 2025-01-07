29d8beb5e373362fdcf4ee5c6835e2e8d7e7a8da
nikita-dev <nikita.baraiya@devdigital.com>
2025-01-07 12:20:43 +0530

    Section 5

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -1,17 +1,33 @@
-let name = 'Max';
-
-if (name === 'Max') {
-  let hobbies = ['Sports', 'Cooking'];
-  console.log(hobbies);
-
+function getName(){
+  return prompt('Your Name','');
 }
-
-function greet() {
-  let age = 30;
-  let name = 'Manuel';
-  console.log(name, age, hobbies);
+function greet(){
+  const userName = getName();
+  console.log('Hello ' + userName);
 }
-
-console.log(name, hobbies);
 
 greet();
+
+
+// let name = 'Max';
+
+// if (name === 'Max') {
+//   let hobbies = ['Sports', 'Cooking'];
+//   console.log(hobbies);
+
+// }
+
+// function greet() {
+//   let age = 30;
+//   let name = 'Manuel';
+//   console.log(name, age, hobbies);
+// }
+
+// console.log(name, hobbies);
+
+// greet();
+
+// 'use strict';
+// userName = 'Max';
+// console.log(userName);
+
